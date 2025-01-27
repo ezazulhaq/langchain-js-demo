e3ca3323547f97b38fb7612ed3a1fc9047ee4367
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-01-27 11:22:02 +0000

    simple prompt template

--- chain.js
+++ chain.js
@@ -1,5 +1,11 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
 
 const model = new ChatGoogleGenerativeAI({
     model: "gemini-1.5-flash"
 })
+
+const promptTemplate = ChatPromptTemplate([
+    ["system", "Your are an export in {area_of_expertise}"],
+    ["user", "{question}"]
+])
