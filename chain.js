376e8f5f2e1e9743bedb2867e8aafca6a9ed155c
ezazulhaq.it <ezazulhaq.it@gmail.com>
2025-01-27 11:45:39 +0000

    sample chaining and output parsed

--- chain.js
+++ chain.js
@@ -1,11 +1,25 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 import { ChatPromptTemplate } from '@langchain/core/prompts'
+import { StringOutputParser } from '@langchain/core/output_parsers'
+import { env } from './config.js';
 
 const model = new ChatGoogleGenerativeAI({
-    model: "gemini-1.5-flash"
-})
+    model: "gemini-1.5-flash",
+    apiKey: env.apiKey
+});
 
 const promptTemplate = ChatPromptTemplate.fromMessages([
     ["system", "Your are an export in {area_of_expertise}"],
     ["user", "{question}"]
-])
+]);
+
+const stringParser = new StringOutputParser();
+
+const chatChain = promptTemplate.pipe(model).pipe(stringParser);
+
+const response = await chatChain.invoke({
+    area_of_expertise: "Computer Engineering",
+    question: "How do CPUs work?"
+})
+
+console.log(response)
