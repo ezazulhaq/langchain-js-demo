f409c4449d85ddc8afe4b418b583fb7c0e9f263c
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2025-02-16 13:06:01 +0530

    using local llm with Embedding

--- chatmodels/local-llm.js
+++ chatmodels/local-llm.js
@@ -1,11 +1,26 @@
-import { ChatOllama } from '@langchain/ollama'
+import { ChatOllama, OllamaEmbeddings } from '@langchain/ollama'
 
-const localModel = new ChatOllama({
-    model: "deepseek-r1:1.5b"
-});
+async function getChatResponse() {
+    const localModel = new ChatOllama({
+        model: "deepseek-r1:1.5b"
+    });
 
-const prompt = "what is 'Thank you very much. See you next time in India'";
+    const prompt = "Translate in Telugu - 'Thank you very much. See you next time in India'";
 
-const response = await localModel.invoke(prompt);
+    const response = await localModel.invoke(prompt);
+    console.log(response.content);
+}
 
-console.log(response.content);
+async function getEmbedResponse() {
+    const embedModel = new OllamaEmbeddings({
+        model: "nomic-embed-text"
+    });
+
+    const text = "what is 'Thank you very much. See you next time in India'";
+
+    const embedResponse = await embedModel.embedQuery(text);
+    console.log(embedResponse);
+}
+
+getChatResponse();
+//getEmbedResponse();
